58d8f98aded02231fc60c65cab898024cfc1e9c7
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-12-20 11:20:03 +0200

    Initializing project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navigation from "./navigation/Navigation.jsx";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Navigation />
+    </>
+  )
+}
+
+export default App
---
+++ src/navigation/Navigation.jsx
@@ -0,0 +1,16 @@
+export default function Navigation() {
+    return(
+        <div className="navbar">
+            <div className="logo-img">
+                <img src=""/>
+            </div>
+            <div className="nav-menu">
+                <ul>
+                    <li>Home</li>
+                    <li>Car List</li>
+                    <li>Contacts</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
